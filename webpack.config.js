a24142cf9a94aca9b6b49c437caa1305a4bffb40
Kenneth Sinay <jksinay@gmail.com>
2025-04-09 10:55:51 +0800

    Update npm scripts for better developer workflow

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ import path from "path";
 const config = {
   entry: ["./out/services/decorate.js", "./out/services/create-nav-bar.js"],
   mode: "development",
+  watch: true,
   output: {
     path: path.resolve("out/dist"),
     filename: "bundle.js",
